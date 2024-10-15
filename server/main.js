eb1751c845ae97bf6864039dad917e1631d26b55
Lucas Axberg <lucas.axberg@gmail.com>
2024-10-15 10:28:37 +0200

    feature: added basic functions for different methods recieved

--- server/main.js
+++ server/main.js
@@ -1,16 +1,36 @@
 const http = require("http");
 
 // Server settings
-const host = 'localhost';
+const host = "localhost";
 const port = 8000;
 
-// Function for listening to interactions
-const request_listener = function(request, response){
-  response.writeHead(200);
-  response.end("My First Server!");
+function on_post_recieved() {
+	console.log("POST request recieved");
+}
+function on_get_recieved() {
+	console.log("GET request recieved");
+}
+function on_other_recieved() {
+	console.log("UNKNOWN request recieved");
 }
 
+// Function for listening to interactions
+const request_listener = function (request, response) {
+	switch (request.method) {
+		case "POST":
+			on_post_recieved();
+			break;
+
+		case "GET":
+			on_get_recieved();
+			break;
+
+		default:
+			on_other_recieved();
+	}
+};
+
 const server = http.createServer(request_listener);
-server.listen(port, host, ()=> {
-  console.log(`Server is running on http://${host}:${port}`)
-})
+server.listen(port, host, () => {
+	console.log(`Server is running on http://${host}:${port}`);
+});
