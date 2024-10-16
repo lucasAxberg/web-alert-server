86862e5391d8746bb2e2d682096b1416d577c538
Lucas Axberg <lucas.axberg@gmail.com>
2024-10-16 10:49:55 +0200

    feature: function for reading files through streams

--- server/main.js
+++ server/main.js
@@ -1,15 +1,36 @@
+// Modules
 const http = require("http");
+const fs = require("node:fs");
 
 // Server settings
 const host = "localhost";
 const port = 8000;
+const data_path = "./data.json";
+
+function read_file(filePath) {
+	// Create a file stream with utf-8 encoding
+	const file_stream = fs.createReadStream(filePath, "utf-8");
+
+	// On read error, print the error in the console
+	file_stream.on("error", (error) => {
+		console.log(`error: ${error.message}`);
+	});
+
+	// On recieved data chunk, print the chunk
+	file_stream.on("data", (chunk) => {
+		console.log(chunk);
+	});
+}
 
 function on_post_recieved() {
 	console.log("POST request recieved");
 }
+
 function on_get_recieved() {
 	console.log("GET request recieved");
+	read_file(data_path);
 }
+
 function on_other_recieved() {
 	console.log("UNKNOWN request recieved");
 }
