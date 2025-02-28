bf416d548957ad99d4294da3e6538e708be67ec7
Lucas Axberg <lucas.axberg@gmail.com>
2025-02-28 10:54:30 +0100

    new: simple scraper that gets value from url and Xpath

---
+++ scraper/scraper.js
@@ -0,0 +1,24 @@
+const playwright = require('playwright');
+
+async function get_value(url, path) {    // Example: get_value("https://www.webhallen.com/", '//div[@class="footer-wrapperi"]')
+  // Launch a non-headless browser
+  const browser = await playwright.chromium.launch({headless: true});
+  const context = await browser.newContext();
+
+  // Go to the url and get the elements matching the path
+  const page = await context.newPage();
+  await page.goto(url);
+  const element = await page.locator(path).all();
+
+  // Make sure there only is one element
+  if (element.length === 0 || element.length > 1){
+    console.log("Path may be broken after page update, consider updating the path")
+    await browser.close();
+    return ""
+  }
+
+  // Return the value of the only item in the array
+  const return_value = await element[0].innerHTML()
+  await browser.close();
+  return return_value
+}
