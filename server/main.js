0d4581807e057525e4031bec760c461a790a65a3
Lucas Axberg <lucas.axberg@gmail.com>
2025-03-12 09:29:57 +0100

    feat: set server to listen to different endpoints and created function for /add

--- server/main.js
+++ server/main.js
@@ -2,7 +2,7 @@
 const http = require("http");
 const { resolve } = require('node:path')
 const { spawn } = require('child_process')
-const {read_file, update_file} = require("./functions.js")
+const {read_file, update_file, write_file} = require("./functions.js")
 const {data_path, host, port} = require("../data/config.js")
 
 function on_post_recieved(request, response) {
@@ -81,18 +81,79 @@ function on_get_recieved(response, request) {
 		});
 }
 
+function on_add(request, response) {
+	// Add all data chunks into one array
+	const chunks = [];
+	request.on("data", (chunk) => {
+		chunks.push(chunk);
+	});
+
+	request.on("end", () => {
+
+		// Responds with error if wrong method was used
+		if (request.method !== "POST") {
+			response.writeHead(405, {'Content-Type':'text/plain'})
+			response.end(`Method "${request.method}" is not allowed at endpoint "/add"`)
+			return
+		}
+
+		try {
+			// Read and parse the data_file	
+			read_file(data_path)
+			.then((text_data) => JSON.parse(text_data))
+			.then((data_object) => {
+
+				// Joins the data and updates the file
+				const complete_data = JSON.parse(chunks.join(""));
+				data_object[Object.keys(data_object).length] = complete_data
+				write_file(data_path, data_object)
+
+				// Responds on successful data recieved
+				response.setHeader('Access-Control-Allow-Origin', '*') // Allows requests from all origins (TODO: Fix so not all)
+				response.writeHead(200, {'Content-Type':'text/plain'})
+				response.end('Data has been recieved successfully')
+			})
+	
+		} catch (error) {
+			
+			// Respond properly to errors
+			if (error.name == "SyntaxError"){
+				
+				// Responds on failed data convertion
+				response.writeHead(400, {'Content-Type':'text/plain'})
+				response.end('Data is not in proper JSON format')
+			} else {
+
+				// Responds with the servers error message on other error
+				response.writeHead(400, {'Content-Type':'text/plain'})
+				response.end(`Server error:\n${error}`)
+			}
+		}
+	});
+}
+
 function on_other_recieved() {
 	print_log("UNKNOWN request recieved");
 }
 
 // Function for listening to interactions
 const request_listener = function (request, response) {
-	switch (request.method) {
-		case "POST":
+	const url = new URL(`http://${host}${request.url}`)
+	console.log(url.pathname)
+	switch (url.pathname) {
+		case "/update":
 			on_post_recieved(request, response);
 			break;
 
-		case "GET":
+		case "/remove":
+			on_post_recieved(request, response);
+			break;
+
+		case "/add":
+			on_add(request, response)
+			break;
+		
+		case "/data":
 			on_get_recieved(response, request);
 			break;
 
